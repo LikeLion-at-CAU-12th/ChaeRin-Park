8b76727f44617e655ebc3a73418f8dddae010ce5
chaelin920 <162853583+chaelin920@users.noreply.github.com>
2024-03-21 22:29:10 +0900

    feat : Delete posts directory

--- posts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from posts.views import *
-
-urlpatterns = [
-    path('', index, name = 'index'),
-    path('page', index, name='my-page'),
-    path('introduction', codeReviewer, name='codeReviewer'),
-]
