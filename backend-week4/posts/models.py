924471d899ded886dc7bfd585732cb2fe39e6310
chaelin <idealisrn@naver.com>
2024-03-28 09:51:48 +0900

    docs : comment models.py에  추가

--- backend-week4/posts/models.py
+++ backend-week4/posts/models.py
@@ -16,8 +16,15 @@ class Post(BaseModel):
         ('ETC', '기타')
     )
 
-    id = models.AutoField(primary_key=True)
+    post_id = models.AutoField(primary_key=True)
     title = models.CharField(verbose_name="제목", max_length=20)
     content = models.TextField(verbose_name="내용")
     writer = models.CharField(verbose_name="작성자", max_length=10)
     category = models.CharField(choices=CHOICES, max_length=20)
+
+class Comment(BaseModel):
+
+    comment_id = models.AutoField(primary_key=True)
+    post_id = models.IntegerField(verbose_name="게시글id")
+    writer = models.CharField(verbose_name="작성자", max_length=10)
+    content = models.TextField(verbose_name="내용")
