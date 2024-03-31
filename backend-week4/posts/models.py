dbbb28c9ee54a3675b861033d8e04571b9a1bd9c
chaelin <idealisrn@naver.com>
2024-03-31 22:01:30 +0900

    feat : 해시태그 기능 추가

--- backend-week4/posts/models.py
+++ backend-week4/posts/models.py
@@ -8,6 +8,7 @@ class BaseModel(models.Model):
     class Meta:
         abstract = True
 
+# 게시글 테이블
 class Post(BaseModel):
 
     CHOICES = (
@@ -22,9 +23,23 @@ class Post(BaseModel):
     writer = models.CharField(verbose_name="작성자", max_length=10)
     category = models.CharField(choices=CHOICES, max_length=20)
 
+# 댓글 테이블
 class Comment(BaseModel):
 
     comment_id = models.AutoField(primary_key=True)
     post_id = models.ForeignKey(Post, verbose_name="게시글", on_delete=models.CASCADE)
     writer = models.CharField(verbose_name="작성자", max_length=10)
     content = models.TextField(verbose_name="내용")
+
+# 해시태그 테이블
+class Hashtag(BaseModel):
+
+    tag_id = models.AutoField(primary_key=True)
+    content = models.CharField(verbose_name="해시태그", max_length=20)
+    post_id = models.ManyToManyField(Post, through='HashtagPosts')
+
+# 해시태그와 게시글의 중간 테이블
+class HashtagPosts(BaseModel):
+    
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    hashtag = models.ForeignKey(Hashtag, on_delete=models.CASCADE)
