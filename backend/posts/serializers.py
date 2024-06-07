0ed3ac1e13ced1f0f7c7623743ae6a09b663bdd2
cherrynniii <chaelin20760@gmail.com>
2024-06-08 01:57:23 +0900

    feat: png 파일 업로드 시 오류

--- backend/posts/serializers.py
+++ backend/posts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Post, Comment
+import os
 
 class PostSerializer(serializers.ModelSerializer):
 
@@ -15,6 +16,15 @@ class PostSerializer(serializers.ModelSerializer):
 
     # 읽기 전용 필드 지정
     # read_only_fields = ['writer']
+  
+  def validate_image(self, value):
+    # ext에 이미지의 확장자명을 담기
+    ext = os.path.splitext(value.name)[1][1:]
+    # 확장자명을 소문자로 통일
+    ext = ext.lower()
+    # 확장자명이 png이면 에러
+    if ext in ['png']:
+      raise serializers.ValidationError(f'Unsupported file extension: .{ext}.')
 
 class CommentSerializer(serializers.ModelSerializer):
   class Meta:
